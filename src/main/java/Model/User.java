84a733ba40ae605eb8e34154a8199efece9f0b3c
khareakanksha <khare.akanksha101@gmail.com>
2024-08-30 18:40:36 +0530

    .mvn added.

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,14 @@
+package Model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+}
---
+++ src/test/java/com/ak/book/managment/user/service/BookManagmentUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.book.managment.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagmentUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
